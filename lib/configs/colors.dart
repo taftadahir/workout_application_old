2bd2a39847e35a3908411c60a45c3b75a2d33eec
taftadjani <taftadjani@gmail.com>
2022-02-01 02:01:41 +0100

    Colors Configuration - v1
    - [x] t1 - Create a variable called *primaryColor*
    - [x] t2 - Create a variable called *whiteColor*
    - [x] t3 - Create a variable called *whiteLightColor*
    - [x] t4 - Create a variable called *blackColor*
    - [x] t5 - Create a variable called *blackLightColor*
    - [x] t6 - Create a variable called *greyColor*
    - [x] t7 - Create a variable called *errorColor*
    - [x] t8 - Create a variable called *successColor*

---
+++ lib/configs/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF545FFF);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color whiteLightColor = Color(0xFFF3F4F8);
+const Color blackColor = Color(0xFF000000);
+const Color blackLightColor = Color(0xFF000000);
+const Color greyColor = Colors.grey;
+const Color errorColor = Colors.red;
+const Color successColor = Colors.green;
