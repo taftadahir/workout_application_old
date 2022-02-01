72ac236a228e8390807536b3e1eb67c945dceef8
taftadjani <taftadjani@gmail.com>
2022-02-01 01:49:58 +0100

    Onboarding Component - v1
    - [x] t1 - Create a *onboarding_component.dart* file in *lib > views > components*
    - [x] t2 - Create a *stateless* widget called *OnboardingComponent*
    - [x] t3 - Add Properties constructors
    - [x] t4 - Add code

---
+++ lib/views/components/onboarding_component.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:workout_application_development/models/onboarding_model.dart';
+
+class OnboardingComponent extends StatelessWidget {
+  final OnboardingModel data;
+
+  const OnboardingComponent({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SvgPicture.asset(
+          'assets/images/${data.image}',
+          height: 200,
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 40.0, 0, 16.0),
+          child: Text(
+            data.title,
+            style: const TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+        Text(
+          data.description,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w400,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+}
