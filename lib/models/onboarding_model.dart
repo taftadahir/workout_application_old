07c5e90f44a60c2fa16da2633a5129a87c47cfdc
taftadjani <taftadjani@gmail.com>
2022-02-01 01:44:26 +0100

    Onboarding Model - v1
    - [x] t1 - Create a *onboarding_model.dart* in *models*
    - [x] t2 - Create a *class* called *OnboardingModel*
    - [x] t3 - Add properties and constructors

---
+++ lib/models/onboarding_model.dart
@@ -0,0 +1,11 @@
+class OnboardingModel {
+  String image;
+  String title;
+  String description;
+
+  OnboardingModel({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
