5e9009e65e631a1c7bcf7985d6b044b916acbc2a
taftadjani <taftadjani@gmail.com>
2022-02-04 00:08:42 +0100

    Onboarding Screen - v2
    - [x] t9 - Make sure the onboarding screen take the whole height
    - [x] t10 - Make sure in smaller screen height, there are no error showing
    - [x] t11 - Create a method called *getOnboardingScreenContent* which contain all repeting code.

--- lib/views/screens/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen.dart
@@ -34,129 +34,132 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height * .95 < 700.0
-                ? null
-                : MediaQuery.of(context).size.height * .95,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    left: 8.0,
-                    right: 16.0,
-                    bottom: 40.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      currentIndex == 0
-                          ? Container()
-                          : IconButton(
-                              icon:
-                                  const Icon(Icons.arrow_back_ios_new_rounded),
-                              color: blackLightColor,
-                              onPressed: () {
-                                _controller.previousPage(
-                                  duration: const Duration(microseconds: 100),
-                                  curve: Curves.bounceIn,
-                                );
-                              },
-                            ),
-                      currentIndex == datas.length - 1
-                          ? Container()
-                          : TextButton(
-                              onPressed: () {
-                                _controller.jumpToPage(
-                                  datas.length - 1,
-                                );
-                              },
-                              child: const Text(
-                                'SKIP',
-                                style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.w600,
-                                  color: blackLightColor,
-                                ),
-                              ),
-                            ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 350.0,
-                  child: PageView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    controller: _controller,
-                    onPageChanged: (int index) {
-                      setState(() {
-                        currentIndex = index;
-                      });
-                    },
-                    itemCount: datas.length,
-                    itemBuilder: (_, index) => Padding(
-                      padding: const EdgeInsets.only(
-                        left: 24,
-                        right: 24,
-                      ),
-                      child: OnboardingComponent(data: datas[index]),
-                    ),
-                  ),
-                ),
-                Column(
+        child: MediaQuery.of(context).size.height < 568.0
+            ? Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: ListView(
                   children: [
-                    Container(
-                      margin: const EdgeInsets.only(top: 40.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(
-                          datas.length,
-                          (index) => DotComponent(
-                            currentIndex: currentIndex,
-                            index: index,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 48.0,
-                      margin: const EdgeInsets.only(
-                        top: 32.0,
-                        left: 24.0,
-                        right: 24.0,
-                        bottom: 24.0,
-                      ),
-                      width: double.infinity,
-                      child: PrimaryButtonComponent(
-                        text: currentIndex == datas.length - 1
-                            ? 'CONTINUE'
-                            : 'NEXT',
-                        onPressed: () {
-                          if (currentIndex == datas.length - 1) {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => const Scaffold(),
-                              ),
-                            );
-                          } else {
-                            _controller.nextPage(
-                              duration: const Duration(microseconds: 100),
-                              curve: Curves.bounceIn,
-                            );
-                          }
-                        },
-                      ),
-                    ),
+                    ...getOnboardingScreenContent(),
                   ],
-                )
-              ],
-            ),
-          ),
-        ),
+                ),
+              )
+            : Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ...getOnboardingScreenContent(),
+                ],
+              ),
       ),
     );
   }
+
+  List<Widget> getOnboardingScreenContent() => [
+        Container(
+          margin: const EdgeInsets.only(
+            left: 8.0,
+            right: 16.0,
+            bottom: 40.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              currentIndex == 0
+                  ? Container()
+                  : IconButton(
+                      icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                      color: blackLightColor,
+                      onPressed: () {
+                        _controller.previousPage(
+                          duration: const Duration(microseconds: 100),
+                          curve: Curves.bounceIn,
+                        );
+                      },
+                    ),
+              currentIndex == datas.length - 1
+                  ? Container()
+                  : TextButton(
+                      onPressed: () {
+                        _controller.jumpToPage(
+                          datas.length - 1,
+                        );
+                      },
+                      child: const Text(
+                        'SKIP',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: blackLightColor,
+                        ),
+                      ),
+                    ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 350.0,
+          child: PageView.builder(
+            physics: const BouncingScrollPhysics(),
+            controller: _controller,
+            onPageChanged: (int index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            itemCount: datas.length,
+            itemBuilder: (_, index) => Padding(
+              padding: const EdgeInsets.only(
+                left: 24,
+                right: 24,
+              ),
+              child: OnboardingComponent(data: datas[index]),
+            ),
+          ),
+        ),
+        Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  datas.length,
+                  (index) => DotComponent(
+                    currentIndex: currentIndex,
+                    index: index,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 48.0,
+              width: double.infinity,
+              margin: const EdgeInsets.only(
+                top: 32.0,
+                left: 24.0,
+                right: 24.0,
+                bottom: 32.0,
+              ),
+              child: PrimaryButtonComponent(
+                text: currentIndex == datas.length - 1 ? 'CONTINUE' : 'NEXT',
+                onPressed: () {
+                  if (currentIndex == datas.length - 1) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const Scaffold(),
+                      ),
+                    );
+                  } else {
+                    _controller.nextPage(
+                      duration: const Duration(microseconds: 100),
+                      curve: Curves.bounceIn,
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ];
 }
