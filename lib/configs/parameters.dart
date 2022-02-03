c3c47238879d82d52d9fbf1c03bfa96b92073b7f
taftadjani <taftadjani@gmail.com>
2022-02-04 00:57:37 +0100

    Parameters Configuration - v1
    - [x] t1 - Add *prmAppName* var containing the app name
    - [x] t2 - Adding all colors used in the app
    	- [x] Colors are nullable

---
+++ lib/configs/parameters.dart
@@ -0,0 +1,14 @@
+// Application name
+import 'package:flutter/material.dart';
+
+const prmAppName = 'Workout Application';
+
+// Application Colors
+const Color? prmPrimaryColor = Color(0xFF545FFF);
+const Color? prmWhiteColor = Color(0xFFFFFFFF);
+const Color? prmWhiteLightColor = Color(0xFFF3F4F8);
+const Color? prmBlackColor = Color(0xFF000000);
+const Color? prmBlackLightColor = Color(0xFF000000);
+const Color? prmGreyColor = Colors.grey;
+const Color? prmErrorColor = Colors.red;
+const Color? prmSuccessColor = Colors.green;
