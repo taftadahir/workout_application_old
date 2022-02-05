ad28c08fe6f2e76cdf9dba768c3f5c1c67a29654
taftadjani <taftadjani@gmail.com>
2022-02-05 23:21:11 +0100

    Themes Configuration - v2
    - [x] t2 - Create a var called *bnbSelectedIconTheme* which is *BottomNavigationBar* selectedIconTheme and get their value from *bottom_bar_component.dart*
    - [x] t3 - Create a var called *bnbUnselectedIconTheme* which is *BottomNavigationBar* unselectedIconTheme and get their value from *bottom_bar_component.dart*
    - [x] tg1 - Default theme data
    	- [x] t4 - Create a var called *outlineInputBorder*
    	- [x] t5 - Add the input decoration value and remove the *primarySwatch*

--- lib/configs/themes.dart
+++ lib/configs/themes.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:workout_application_development/configs/colors.dart';
+import 'package:workout_application_development/configs/styles.dart';
+
+const OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+  borderSide: BorderSide(
+    color: whiteLightColor,
+    width: 0.0,
+  ),
+);
 
 // Default theme data
 ThemeData defaultThemeData = ThemeData(
-  primarySwatch: Colors.blue,
+  inputDecorationTheme: InputDecorationTheme(
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    border: outlineInputBorder,
+    enabledBorder: outlineInputBorder,
+    errorBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    focusedErrorBorder: outlineInputBorder,
+    disabledBorder: outlineInputBorder,
+    floatingLabelStyle: fieldLabelStyle,
+    errorStyle: fieldErrorStyle,
+    hintStyle: fieldHintStyle,
+    labelStyle: fieldLabelStyle,
+  ),
+);
+
+// BottomNavigationBarSelectedIconTheme = bnbSelectedIconTheme
+const IconThemeData bnbSelectedIconTheme = IconThemeData(
+  color: primaryColor,
+  opacity: 1,
+  size: 24.0,
+);
+
+// BottomNavigationBarUnselectedIconTheme = bnbUnselectedIconTheme
+const IconThemeData bnbUnselectedIconTheme = IconThemeData(
+  color: blackLightColor,
+  opacity: 1,
+  size: 24.0,
 );
