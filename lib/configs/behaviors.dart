691b05e718c8478e2954a3af1c1af07eca2c7da0
taftadjani <taftadjani@gmail.com>
2022-02-05 23:18:58 +0100

    Behaviors Configuration - v2
    - [x] t2 - Create a var called *bottomNavigationBarType* which hold a *BottomNavigationBarType.fixed*

--- lib/configs/behaviors.dart
+++ lib/configs/behaviors.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const ScrollPhysics? physics = BouncingScrollPhysics();
+const BottomNavigationBarType bottomNavigationBarType =
+    BottomNavigationBarType.fixed;
