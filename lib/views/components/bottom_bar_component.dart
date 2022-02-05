7df718e69ae3b84b589d8b3f96c1cdbd2f96653e
taftadjani <taftadjani@gmail.com>
2022-02-05 01:02:02 +0100

    Bottom bar Component - v1
    - [x] t1 - Create a *bottom_bar_component.dart* file in *views > components*
    - [x] t2 - Create a stateless widget called *BottomBarComponent*
    - [x] t3 - Adding the required code to show the bottom bar us designed
    - [x] t4 - Add a var called *selectedIndex*
    - [x] t5 - Add a function *onTap* for the items

---
+++ lib/views/components/bottom_bar_component.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:workout_application_development/configs/colors.dart';
+
+class BottomBarComponent extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int index) onTap;
+
+  const BottomBarComponent({
+    Key? key,
+    required this.selectedIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
+      currentIndex: selectedIndex,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      selectedFontSize: 0,
+      unselectedFontSize: 0,
+      unselectedItemColor: blackLightColor,
+      backgroundColor: whiteColor,
+      selectedIconTheme: const IconThemeData(
+        color: primaryColor,
+        opacity: 1,
+        size: 24.0,
+      ),
+      unselectedIconTheme: const IconThemeData(
+        color: blackLightColor,
+        opacity: 1,
+        size: 24.0,
+      ),
+      onTap: (index) => onTap(index),
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.date_range_rounded),
+          label: 'Routine',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_border_rounded),
+          label: 'Favorite',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings_outlined),
+          label: 'Setting',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.history_rounded),
+          label: 'History',
+        ),
+      ],
+    );
+  }
+}
