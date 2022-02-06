ccd25595acc8c880e51c9933118baf82ea03a74c
taftadjani <taftadjani@gmail.com>
2022-02-06 17:05:21 +0100

    Behaviors Configuration - v3
    - [x] t3 - Add as a comment all possible values of *physics*

--- lib/configs/behaviors.dart
+++ lib/configs/behaviors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 const ScrollPhysics? physics = BouncingScrollPhysics();
+// const ScrollPhysics? physics = ClampingScrollPhysics();
+// const ScrollPhysics? physics = null;
+
 const BottomNavigationBarType bottomNavigationBarType =
     BottomNavigationBarType.fixed;
