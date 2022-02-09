60096444ea3e8203a0c25ed588464d92ff77cf16
taftadjani <taftadjani@gmail.com>
2022-02-09 18:27:54 +0100

    Workout Application Entry task - v2
    - [x] t8 - Create a var *_appbars* that hold the appbars and take the appbar from others screens
    - [x] t9 - Add appbar property to the scaffold

--- lib/workout_application.dart
+++ lib/workout_application.dart
@@ -1,19 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:workout_application_development/configs/colors.dart';
 import 'package:workout_application_development/configs/parameters.dart';
+import 'package:workout_application_development/configs/styles.dart';
 import 'package:workout_application_development/configs/themes.dart';
+import 'package:workout_application_development/views/components/app_bar_component.dart';
+import 'package:workout_application_development/views/components/bottom_bar_component.dart';
 import 'package:workout_application_development/views/screens/home_screen.dart';
-import 'package:workout_application_development/views/screens/login_screen.dart';
-import 'package:workout_application_development/views/screens/onboarding_screen.dart';
-import 'package:workout_application_development/views/screens/register_screen.dart';
+import 'package:workout_application_development/views/screens/routine_screen.dart';
+import 'dart:math' as math;
 
-class WorkoutApplication extends StatelessWidget {
+class WorkoutApplication extends StatefulWidget {
   const WorkoutApplication({Key? key}) : super(key: key);
+
+  @override
+  State<WorkoutApplication> createState() => _WorkoutApplicationState();
+}
+
+class _WorkoutApplicationState extends State<WorkoutApplication> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const RoutineScreen(),
+    Container(),
+    Container(),
+    Container(),
+  ];
+
+
+  final List<AppBar> _appbars = [
+
+    // Home screen Appbar
+    AppBarComponent.getAppBarComponent(
+      title: Text(
+        'Home Screen',
+        style: getTextStyle(
+          color: blackColor,
+        ),
+      ),
+      leading: IconButton(
+        onPressed: () {},
+        icon: Transform.rotate(
+          angle: math.pi / 2,
+          child: const Icon(
+            Icons.bar_chart_rounded,
+            color: blackColor,
+          ),
+        ),
+      ),
+    ),
+
+    // Routine screen Appbar
+    AppBarComponent.getAppBarComponent(
+      title: Text(
+        'Routine',
+        style: getTextStyle(
+          color: blackColor,
+        ),
+      ),
+      leading: IconButton(
+        onPressed: () {},
+        icon: Transform.rotate(
+          angle: math.pi / 2,
+          child: const Icon(
+            Icons.bar_chart_rounded,
+            color: blackColor,
+          ),
+        ),
+      ),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(top: 8.0, bottom: 8.0, right: 24.0),
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Start',
+              style: getTextStyle(
+                color: blackColor,
+              ),
+            ),
+            style: getButtonStyle(
+              fullWidth: false,
+              radius: 8.0,
+              backgroundColor: whiteLightColor,
+            ),
+          ),
+        ),
+      ],
+    ),
+
+    // Favorite screen Appbar
+    AppBarComponent.getAppBarComponent(),
+
+    // Setting screen Appbar
+    AppBarComponent.getAppBarComponent(),
+
+    // History screen Appbar
+    AppBarComponent.getAppBarComponent(),
+  ];
 
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: prmAppName,
         theme: defaultThemeData,
         debugShowCheckedModeBanner: false,
-        home: const RegisterScreen(),
+        home: Scaffold(
+          backgroundColor: whiteColor,
+          appBar: _appbars[_selectedIndex],
+          bottomNavigationBar: BottomBarComponent(
+            selectedIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+          body: _screens[_selectedIndex],
+        ),
       );
 }
