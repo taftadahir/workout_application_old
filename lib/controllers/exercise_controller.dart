464244bc0b22a98c7678b0c68b27ef033e4f9252
taftadjani <taftadjani@gmail.com>
2022-02-12 00:19:22 +0100

    Create an exercise controller - v1
    - [x] t1 - Create *exercise_controller.dart* in *controller*
    - [x] t2 - Add a class *ExerciseController*
    - [x] t3 - Add a static method *getMyFavoriteExercise* which get a list of Exercise

---
+++ lib/controllers/exercise_controller.dart
@@ -0,0 +1,106 @@
+import 'package:workout_application_development/models/exercise_model.dart';
+import 'package:workout_application_development/views/components/exercise_card_component.dart';
+
+class ExerciseController {
+  static List<ExerciseModel> getMyFavoriteExercise() => [
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up and Rotation',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+        ExerciseModel(
+          id: 1,
+          name: 'Push Up',
+          published: true,
+          sysId: 1,
+          userId: 1,
+          description:
+              '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim tempus enim blandit lacinia at. Tincidunt sollicitudin amet convallis quam consectetur eget auctor. Luctus pretium curabitur in dolor bibendum senectus aenean. Nulla vulputate morbi egestas ultricies ''',
+        ),
+      ];
+}
