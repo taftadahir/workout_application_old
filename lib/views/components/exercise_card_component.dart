45247ff841ad4e97d491be8b4540d32a18912528
taftadjani <taftadjani@gmail.com>
2022-02-12 00:18:38 +0100

    Exercise Card Component - v2
    - [x] t4 - Add onclick to open the *ExerciseBottomSheetComponent*
    - [x] t5 - Add the image
    - [x] t6 - Add the exercise name
    - [x] t7 - Add parameters
    	- [x] exercise
    - [x] t8 - Use the *sys_id* as the image identifier
    - [x] t9 - Use the exercise name

--- lib/views/components/exercise_card_component.dart
+++ lib/views/components/exercise_card_component.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:workout_application_development/configs/colors.dart';
+import 'package:workout_application_development/configs/styles.dart';
+import 'package:workout_application_development/models/exercise_model.dart';
+import 'package:workout_application_development/views/components/exercise_bottom_sheet_component.dart';
 
 class ExerciseCardComponent extends StatelessWidget {
-  const ExerciseCardComponent({Key? key}) : super(key: key);
+  final ExerciseModel exercise;
+
+  const ExerciseCardComponent({
+    Key? key,
+    required this.exercise,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      decoration: BoxDecoration(
-        color: greyColor,
-        borderRadius: BorderRadius.circular(8.0),
+    return GestureDetector(
+      onTap: () => showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(16.0),
+          ),
+        ),
+        builder: (_) => ExerciseBottomSheetComponent(exercise: exercise,),
+      ),
+      child: Container(
+        height: 144,
+        width: 120,
+        decoration: BoxDecoration(
+          color: whiteLightColor,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(8.0)),
+              child: Image.asset(
+                'assets/images/exercises/${exercise.sysId}.jpg',
+                fit: BoxFit.cover,
+                height: 112.0,
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    exercise.name,
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: getTextStyle(
+                      color: blackColor,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14.0,
+                    ),
+                    maxLines: 1,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
