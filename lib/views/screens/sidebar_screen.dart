d4a7d754546a4f0387ed1c76a583ccbbc4b917b3
taftadjani <taftadjani@gmail.com>
2022-02-12 21:17:53 +0100

    Sidebar Screen - v1
    - [x] t1 - Create *sidebar_screen.dart*
    - [x] t2 - Add the *drawer* property to the Scaffold in *workout_application.dart*
    - [x] t3 - Create a stateless widget *SidebarScreen*
    - [x] t4 - Return a *Drawer* widget
    - [x] t5 - Add the code to open the *drawer*
    - [x] t6 - Add the rest of the code

---
+++ lib/views/screens/sidebar_screen.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:workout_application_development/configs/colors.dart';
+import 'package:workout_application_development/configs/parameters.dart';
+import 'package:workout_application_development/configs/styles.dart';
+import 'package:workout_application_development/views/components/sidebar_item_component.dart';
+import 'package:workout_application_development/workout_application.dart';
+
+class SidebarScreen extends StatelessWidget {
+  const SidebarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: whiteColor,
+      elevation: 0.0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 24.0,
+          horizontal: 24.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).viewPadding.top + 24.0,
+                ),
+                ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(8.0),
+                  ),
+                  child: Image.asset(
+                    'assets/images/exercises/1.jpg',
+                    fit: BoxFit.cover,
+                    height: 80.0,
+                    width: 80.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  'Jane Doe',
+                  style: getTextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: blackColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  'jane.doe@gmail.com',
+                  style: getTextStyle(
+                    color: blackColor,
+                    fontSize: 12.0,
+                  ),
+                ),
+                const Divider(
+                  color: whiteLightColor,
+                  thickness: 1.0,
+                  height: 40.0,
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SidebarItemComponent(
+                  icon: Icons.person_rounded,
+                  title: 'Profile',
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => WorkoutApplication(
+                          index: 0,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SidebarItemComponent(
+                  icon: Icons.history_rounded,
+                  title: 'History',
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => WorkoutApplication(
+                          index: 4,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SidebarItemComponent(
+                  icon: Icons.favorite_border_rounded,
+                  title: 'Favorite',
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => WorkoutApplication(
+                          index: 2,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SidebarItemComponent(
+                  icon: Icons.settings_rounded,
+                  title: 'Settings',
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => WorkoutApplication(
+                          index: 3,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SidebarItemComponent(
+                  icon: Icons.bubble_chart_rounded,
+                  title: 'About Us',
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                SidebarItemComponent(
+                  icon: Icons.logout,
+                  title: 'Logout',
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  height: 32.0,
+                ),
+              ],
+            ),
+            Text(
+              prmAppName,
+              style: getTextStyle(
+                color: blackColor,
+                fontSize: 12.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
